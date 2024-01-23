0ca5b98f198432061657b22af2b84b65ff067aef
ra397 <rabialaya@gmail.com>
2024-01-23 12:07:06 -0600

    implemented echo function

--- echo.py
+++ echo.py
@@ -1,7 +1,26 @@
 #echo.py
 
+
 def echo(text: str, repititions: int = 3) -> str:
-    pass
+    num_chars = 0
+    last_three_chars = ''
+
+    # iterate from the end of the string
+    for c in reversed(text):
+        if c not in (' ', '.', '?', '!') and num_chars < 3:
+            last_three_chars += c
+            num_chars += 1
+    final_echo = ""
+    for i in range(num_chars):
+        final_echo = final_echo + last_three_chars[i:]
+        if i < 2:
+            final_echo += '\n'
+        else:
+            final_echo += '\n.'
+    return final_echo
+
+    
+    
 
 if __name__ == "__main__":
     text = input("Yell something at a mountain: ")
