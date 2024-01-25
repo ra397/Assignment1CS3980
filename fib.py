8edbf97256338a3346e8169fd0a106b382a80c0b
ra397 <rabialaya@gmail.com>
2024-01-25 12:47:06 -0600

    save

--- fib.py
+++ fib.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 def timer(func):
     def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        curr_exec_time = end - start
-        print(f"Finished in {curr_exec_time:0.4f}s: f({args[0]}) -> {result}")
-        fib_inputs.append(args[0])
-        execution_times.append(curr_exec_time)
+        start = time.time() # take the start time
+        result = func(*args, **kwargs) # execute the function passed in and store the result
+        end = time.time() # take the end time
+        curr_exec_time = end - start # calculate the execution time
+        print(f"Finished in {curr_exec_time:0.4f}s: fib({args[0]}) -> {result}") # print the execution time and the solution
+        fib_inputs.append(args[0]) # append the input to inputs list, this will be the x-axis of our graph
+        execution_times.append(curr_exec_time) # append the execution time of the input, this will be the y-axis of our graph
         return result
     return wrapper
 
